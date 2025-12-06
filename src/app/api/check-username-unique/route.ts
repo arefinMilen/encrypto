2741ec11d3f5e55661cc02bf68f73138c758c29a
arefinMilen <arefin15-5279@diu.edu.bd>
2025-12-06 23:52:37 +0600

    fixed export issues and rename the import name of user schema

--- src/app/api/check-username-unique/route.ts
+++ src/app/api/check-username-unique/route.ts
@@ -1,10 +1,10 @@
 import dbConnect from "@/lib/dbConnect";
 import UserModel from "@/model/User";
 import {z} from "zod";
-import {usernameValidation} from "@/schemas/signUpSchema"
+import {uservalidationSchema} from "@/schemas/signUpSchema"
 
 const UsernameQuerySchema = z.object({
-    username: usernameValidation
+    username: uservalidationSchema
 })
 
 export async function GET(request : Request) {
