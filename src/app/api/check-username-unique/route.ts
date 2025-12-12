bb3acd9bfe8d3c21aa446ba8d3ce9ccb79f51df0
arefinMilen <arefin15-5279@diu.edu.bd>
2025-12-12 18:48:35 +0600

    validate query param for user name is unique or not in try block

--- src/app/api/check-username-unique/route.ts
+++ src/app/api/check-username-unique/route.ts
@@ -10,6 +10,40 @@ const UsernameQuerySchema = z.object({
 export async function GET(request : Request) {
     await dbConnect();
     try {
+        const {searchParams} = new URL(request.url)
+        const queryParam = {
+            username: searchParams.get('username')
+        }
+        //validate query param with zod
+        const result = UsernameQuerySchema.safeParse(queryParam)
+        console.log(result) //todo : remove 
+        if (!result.success) {
+            const usernameErrors = result.error.format().
+            username?._errors || []
+            return Response.json(
+        {
+                    success: false,
+                    message: usernameErrors?.length > 0 
+                    ? usernameErrors.join(', ') 
+                    : "Invalid query parameters" , 
+                }, {status: 400}
+            )
+        }
+        
+        const {username} = result.data
+        const existingVerifiedUser = await UserModel.findOne
+        ({username, isverified: true})
+
+        if (existingVerifiedUser) {
+            return Response.json({
+                success: false,
+                message: 'Username is already taken'
+            }, {status: 400})
+        }
+        return Response.json({
+            success: true,
+            message: 'username is unique'
+        }, {status : 400})
 
     } catch (error) {
         console.error("Error checking username", error)
