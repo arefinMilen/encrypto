adf9263875fb87cc934992d0bbac0e0a11650ba2
arefinMilen <arefin15-5279@diu.edu.bd>
2025-12-14 21:32:00 +0600

    creatd post method route for accept-messages

---
+++ src/app/api/accept-messages/route.ts
@@ -0,0 +1,49 @@
+import { getServerSession } from "next-auth";
+import { authOptions } from "../auth/[...nextauth]/options";
+import dbConnect from "@/lib/dbConnect";
+import UserModel from "@/model/User";
+import { User } from "next-auth";
+
+export async function POST(request: Request) {
+    await dbConnect();
+
+    const session = await getServerSession(authOptions)
+    const user: User = session?.user 
+    if (!session || !session.user) {
+        return Response.json({
+            success: false,
+            message: "Not Authorized "
+        },{status: 401}
+    )
+    }
+    const userId = user._id;
+    const {acceptMessages} = await request.json()
+    try{
+        const updatedUser = await UserModel.findByIdAndUpdate(
+            userId,
+            {isAcceptingMessages: acceptMessages},
+            {new: true}
+        )
+        if(!updatedUser){
+        return  Response.json({
+            success: false,
+            message: "failed to update user status to accept messages"
+          }, {status: 401})    
+        }
+        return Response.json(
+            {
+                success: true,
+                message: "Message acceptance status updated successfully",
+                updatedUser
+            }, 
+            {status: 200})
+
+    } catch (error) {
+        console.log("failed to update user status to accept messages")
+    return Response.json({
+        success: false,
+        message: "Error updating user preferences"
+        
+    }, {status: 500})
+    }
+}
