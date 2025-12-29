6ae4e558df81bd377bacfd159c601b7dc68b3ffb
arefinMilen <arefin15-5279@diu.edu.bd>
2025-12-30 01:14:48 +0600

    apply mongoose aggretion to find user

--- src/app/api/get-messages/route.ts
+++ src/app/api/get-messages/route.ts
@@ -3,6 +3,7 @@ import { authOptions } from "../auth/[...nextauth]/options";
 import dbConnect from "@/lib/dbConnect";
 import UserModel from "@/model/User";
 import { User } from "next-auth";
+import mongoose from "mongoose";
 
 export async function GET(request: Request) {
     await dbConnect();
@@ -17,5 +18,37 @@ export async function GET(request: Request) {
     {status: 401}
 )
     }
-  
+    const userId = new mongoose.Types.ObjectId(user._id);
+    try {
+        const user = await UserModel.aggregate([
+            {$match: {id: userId} },
+            {$unwind: '$messages'},
+            {$sort: {'messages.createdAt': -1}},
+            {$group: {_id: '$_id', messages: {$push: '$messages'}}}
+        ])
+        if (!user || user.length === 0) {
+            return Response.json( {
+                success: false,
+                message: "User not found"
+            },
+            {status: 401}
+        )
+        }
+         return Response.json( {
+                success: true,
+                message: "User[0].messages",
+            },
+            {status: 200}
+        )
+    } catch (error) {
+        console.error("An unexpected error occurred:", error)
+        return Response.json(
+            {
+                success: false,
+                message: "not authenticated"
+            },
+            {status: 500}
+        )
+    }
+    
 }
